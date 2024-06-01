a224e1a55694490766741b8771f9ecb3b7258ab3
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-02 04:07:26 +0600

    Update JavaPythonInterop.java

--- src/main/java/com/example/JavaPythonInterop.java
+++ src/main/java/com/example/JavaPythonInterop.java
@@ -4,20 +4,15 @@ import py4j.GatewayServer;
 
 public class JavaPythonInterop {
     public static void main(String[] args) {
-        GatewayServer gatewayServer = new GatewayServer(new JavaPythonInterop());
+        GatewayServer gatewayServer = new GatewayServer(new PythonGateway());
         gatewayServer.start();
         System.out.println("Gateway Server Started");
 
-        // Assuming the Python script is running and waiting for input
-        System.out.println("Calling Python function...");
+        // Simulating Python call
         PythonGateway pythonGateway = new PythonGateway();
-        int result = pythonGateway.add(10, 5);
+        int result = pythonGateway.callPython(10, 5);
         System.out.println("Result from Python: " + result);
 
         gatewayServer.shutdown();
-    }
-
-    public int add(int a, int b) {
-        return a + b;
     }
 }
