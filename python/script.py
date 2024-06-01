d9be7e171497c1736e2d1c4a368dd77eccc79b37
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-02 03:58:00 +0600

    Create script.py

---
+++ python/script.py
@@ -0,0 +1,11 @@
+from py4j.java_gateway import JavaGateway, GatewayParameters, CallbackServerParameters
+
+class PythonEntryPoint:
+    def add(self, a, b):
+        return a + b
+
+if __name__ == "__main__":
+    gateway = JavaGateway(gateway_parameters=GatewayParameters(port=25333),
+                          callback_server_parameters=CallbackServerParameters(port=25334),
+                          python_server_entry_point=PythonEntryPoint())
+    print("Python server started and waiting for calls...")
