3aa0bf56e8ee289fd91713aaeca42410e83a1ee4
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-02 22:27:45 +0600

    Update script.py

--- python/script.py
+++ python/script.py
@@ -10,7 +10,4 @@ if __name__ == "__main__":
         callback_server_parameters=CallbackServerParameters(port=25334)
     )
     python_entry_point = PythonEntryPoint()
-    gateway.java_gateway_server.resetCallbackClient(gateway.java_gateway_server.getCallbackClient())
     print("Python server started and waiting for calls...")
-
-   
