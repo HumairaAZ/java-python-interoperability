468fd166fef8235d325edb56444df0c629bb884c
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-02 22:16:57 +0600

    Update script.py

--- python/script.py
+++ python/script.py
@@ -5,7 +5,12 @@ class PythonEntryPoint:
         return a + b
 
 if __name__ == "__main__":
-    gateway = JavaGateway(gateway_parameters=GatewayParameters(port=25333),
-                          callback_server_parameters=CallbackServerParameters(port=25334),
-                          python_server_entry_point=PythonEntryPoint())
+    gateway = JavaGateway(
+        gateway_parameters=GatewayParameters(port=25333),
+        callback_server_parameters=CallbackServerParameters(port=25334)
+    )
+    python_entry_point = PythonEntryPoint()
+    gateway.java_gateway_server.resetCallbackClient(gateway.java_gateway_server.getCallbackClient())
     print("Python server started and waiting for calls...")
+
+   
